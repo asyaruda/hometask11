cbbb47aae275f728562e986e8c4a9295a0d76123
Anastasiia Kholod <asyaruda06@gmail.com>
2023-06-14 11:02:33 +0300

    update

--- recursiveFunction.js
+++ recursiveFunction.js
@@ -2,14 +2,22 @@
 function pow(num, degree) {
 	if (degree === 0) {
 	  return 1
-	} else if (degree < 0) {
-	  return 1 / pow(num, -degree)
-	} else {
-	  return num * pow(num, degree - 1)
 	}
+
+	else if (degree === 1) {
+		return num
+	}
+
+	else if (degree > 0) {
+		return num * pow(num, degree - 1)
+	}
+
+	else if (degree < 0) {
+	  return 1 / (num * pow(num, -degree - 1))
+	} 
   }
 
-console.log(pow(7, 3))
+console.log(pow(7, x))
 console.log(pow(4, 3))
-console.log(pow(9, 2))
-console.log(pow(-9, -1))
+console.log(pow(9, 5))
+console.log(pow(-9, -3))
