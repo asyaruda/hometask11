81657c9efb24dc7c6ea97b2e58b56a17c9506c4d
Anastasiia Kholod <asyaruda06@gmail.com>
2023-06-15 23:34:15 +0300

    modified

--- recursiveFunction.js
+++ recursiveFunction.js
@@ -1,23 +1,42 @@
 'use strict'
 function pow(num, degree) {
+	if (typeof num !== 'number' || typeof degree !== 'number') {
+	  throw new Error('Обидва аргумента мають бути числом')
+	}
+  
 	if (degree === 0) {
 	  return 1
-	}
-
-	else if (degree === 1) {
-		return num
-	}
-
-	else if (degree > 0) {
-		return num * pow(num, degree - 1)
-	}
+	} 
 
 	else if (degree < 0) {
-	  return 1 / (num * pow(num, -degree - 1))
+	  return 1 / pow(num, -degree)
 	} 
-  }
 
-console.log(pow(7, x))
-console.log(pow(4, 3))
-console.log(pow(9, 5))
-console.log(pow(-9, -3))
+	else if (degree % 2 === 0) {
+	  return pow(num * num, degree / 2)
+	} 
+
+	else {
+	  return num * pow(num * num, (degree - 1) / 2)
+	}
+  }
+  
+  console.log(pow(2, 3))
+  console.log(pow(5, -2))
+  console.log(pow(10, 'f'))
+  console.log(pow('h', 2))
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
